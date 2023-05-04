002108a541a18731cbeef0aa7387241ed155d104
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-05-04 15:23:01 +0300

    Something changes

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,7 +4,14 @@ import styles from './Content.module.scss'
 const Content = () => {
   return (
     <div className={styles.content}>
-
+      <div className={styles.content__research}>
+        <textarea
+          name=""
+          id=""
+          className={styles.content__research__field}
+        ></textarea>
+        <button className={styles.content__research__submit}>ПОИСК</button>
+      </div>
     </div>
   );
 };
