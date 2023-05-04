134fff8521d90d099bc9ffcd48ddcac5f36686cd
Jesper Nielsen <sermyagin.abcen7@gmail.com>
2023-05-04 16:56:08 +0300

    Added description of posts

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -9,15 +9,27 @@ const Content = () => {
           name=""
           id=""
           className={styles.content__research__field}
+          placeholder="Введите пост для поиска в нем биржевого сленга"
         ></textarea>
         <button className={styles.content__research__submit}>ПОИСК</button>
       </div>
       <div className={styles.content__description}>
-        СД - Совет компании по Дивидендам
-
-        Дивы - дивиденды, часть суммы чистой прибыли акционерного общества, распределяемая между акционерами в соответствии с количеством акций
-
-        Памп - провоцируемый рост акционерами спроса на активы с целью дальнейшей его продажи по высокой цене
+        <p>
+          <span className={styles.content__description__highlight}>СД</span> - Совет компании по Дивидендам
+        </p>
+        <p>
+          <span className={styles.content__description__highlight}>Дивы</span> - дивиденды, часть суммы чистой прибыли акционерного общества, распределяемая между акционерами в соответствии с количеством акций
+        </p>
+        <p>
+          <span className={styles.content__description__highlight}>Памп</span> - провоцируемый рост акционерами спроса на активы с целью дальнейшей его продажи по высокой цене
+        </p>
+      </div>
+      <div className={styles.content__images}>
+        <img src="images/pulse_3d.png" alt=""/>
+      </div>
+      <div className={styles.content__pulse_last_post}>
+        <h3>Рекомендуем прочитать</h3>
+        ...
       </div>
     </div>
   );
